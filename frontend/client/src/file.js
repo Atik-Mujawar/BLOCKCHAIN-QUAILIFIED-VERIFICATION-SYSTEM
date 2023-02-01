69e82d402d12d1d58ca8b0fdfe8f736646298027
doshidev01 <88660808+doshidev01@users.noreply.github.com>
2023-02-01 14:19:29 +0530

    first commit

--- frontend/client/src/file.js
+++ frontend/client/src/file.js
@@ -4,6 +4,47 @@ import React from "react";
 
 /*import { loadBlockchainData, loadWeb3 } from "./Web3helpers"*/
 
+import emailjs from 'emailjs-com'
+
+
+
+const Mailer = () => {
+    const email = localStorage.getItem("email");
+    const account = localStorage.getItem("account");
+    <div className="c">
+            <div>
+                <h3>Your account: {account} </h3>
+                <h3>Your email: {email} </h3>
+                <button
+                    onClick={() => {
+                    localStorage.removeItem("email");
+                    localStorage.removeItem("account");
+                    window.location.reload();
+                    }}
+                    >
+                    {" "}Log out
+
+                </button>
+                
+                
+            </div>
+        </div>
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            "service_le4lrd5",
+            "template_ugb9k9u",
+            e.target,
+            "aVd8spLfEWWnzVmBu"    
+        ).then(res=>{
+            console.log(res);
+        }).catch(err=> console.log(err));
+    }
+
+    
+  }
+
 
 const apiToken =
   "YOUR TOKEN"
@@ -53,8 +94,34 @@ export default function Apps(props) {
         <label for="file">Choose file to upload</label>
         <input type="file" id="input" name="file" multiple />
       </div>
+
+
+ <form className="uploadfile" onSubmit={sendEmail}>
+    <div className="ui divider"></div>
+    <div className="ui form">
+    <div className="field"></div>
+
+    <div className="field">
+                <input
+                    type="email"
+                    name="user_email"
+                    placeholder="Enter the certificate holder's email id"
+                />
+                </div>
+
+                <div className="field">
+                    <input
+                        type="text"
+                        name="message"
+                        value="cid"
+                    />
+                </div>
+                </div>
+            </form>
+
+
       <div id="root">
-        <button onClick={handleUpload}>Submit</button>
+        <button onclick={()=>{ handleUpload(); sendEmail() }}>Submit</button>
         <p id="cid"></p>
         
       </div>
